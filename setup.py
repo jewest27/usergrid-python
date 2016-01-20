9d07735ed5a8d4cfc33e5c6d5ecceebd0041cb21
Jeff West <jwest@apigee.com>
2016-01-20 09:01:56 -0800

    inc version, added urllib3 dependency

--- setup.py
+++ setup.py
@@ -2,19 +2,20 @@ __author__ = 'Jeff West @ ApigeeCorporation'
 
 from setuptools import setup, find_packages
 
-VERSION = '0.0.3'
+VERSION = '0.0.4'
 
 setup(
     name='usergrid',
     version=VERSION,
     description='Usergrid SDK for Python',
     url='http://usergrid.apache.org',
-    download_url="https://codeload.github.com/jwest-apigee/usergrid-python/zip/v0.0.3",
+    download_url="https://codeload.github.com/jwest-apigee/usergrid-python/zip/v0.0.4",
     author='Jeff West',
     author_email='jwest@apigee.com',
     packages=find_packages(),
     install_requires=[
         'requests',
+        'urllib3'
     ],
     entry_points={
     }
